a3eaea03ac7ada137d9a7190846ec8d2727dd86c
Eduardo Llaria Orbegozo <edllaor77@gmail.com>
2024-05-26 20:35:07 +0200

    Load Inicial

---
+++ src/routes/api/groups.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+const {getAllGroups, getGroupById, getGroupsByCreatorId, createGroup} = require('../../controllers/groups.controller');
+
+router.get('/', getAllGroups);
+router.get('/:id', getGroupById);
+
+router.use('/creator_id/:creator_id', getGroupsByCreatorId);
+
+router.post('/',createGroup);
+
+
+module.exports = router;
